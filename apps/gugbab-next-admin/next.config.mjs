10cc59227454dd11797e0965f8036b171d47f81b
puk0806 <puk0806@naver.com>
2024-10-31 08:22:45 +0900

    [next-app] Add Next App Set Up

---
+++ apps/gugbab-next-admin/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@gugbab-integrated-admin-poc/ui-vanilla-extract'],
+};
+
+export default nextConfig;
---
+++ apps/gugbab-next-admin/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Typography } from "@gugbab-integrated-admin-poc/ui-vanilla-extract";
+
+export default function Home() {
+  return (
+    <main>
+      <Typography>test</Typography>
+    </main>
+  );
+}
