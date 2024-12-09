cbd38e9cb8c4f508a1bddd64bae718fc71298e73
puk0806 <puk0806@naver.com>
2024-12-10 08:00:27 +0900

    [app] Add Auth Page

---
+++ apps/gugbab-next-admin/src/app/api/auth/login/route.ts
@@ -0,0 +1,61 @@
+import { headers } from 'next/headers';
+import { type NextRequest } from 'next/server';
+import { apiFetch } from '@app/shared/fetch/utils/lib/fetch';
+import { getEnv } from '@app/shared/common/utils';
+import { ErrorResponse } from '@app/shared/error';
+import { AUTH_COOKIE, TokenResponse, setCookies } from '@app/shared/auth';
+
+/**
+ * 로그인
+ * - basicToken : base64(clientId:secretKey)
+ * - body: {
+ *    grantType: 'password',
+ *    id,
+ *    password,
+ *   }
+ * @param request
+ */
+export async function POST(request: NextRequest) {
+  try {
+    const text = await request.text();
+    const data = JSON.parse(text);
+    const { id, password } = data;
+    const secret = getEnv('SECRET_KEY_USER');
+    const basicToken = btoa(`gugbab:${secret}`);
+
+    // TODO API call
+    const response = await apiFetch.post<TokenResponse>(
+      getEnv('AUTH_URL'),
+      '',
+      {
+        headers: {
+          Authorization: `Basic ${basicToken}`,
+        },
+        data: {
+          grant_type: 'password',
+          id,
+          password,
+        },
+      },
+      headers,
+    );
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: {
+        'Set-Cookie': setCookies([
+          { key: AUTH_COOKIE.ACCESS_TOKEN, value: response.access_token },
+          {
+            key: AUTH_COOKIE.REFRESH_TOKEN,
+            value: response.refresh_token,
+            options: { HttpOnly: true },
+          },
+        ]),
+      },
+    });
+  } catch (error: unknown) {
+    const customError = error as ErrorResponse;
+    return new Response(JSON.stringify(customError), {
+      status: customError.status || 400,
+    });
+  }
+}
---
+++ apps/gugbab-next-admin/src/shared/auth/utils/lib/cookie.ts
@@ -0,0 +1,26 @@
+export type CookieOptions = {
+  Secure?: boolean;
+  Path?: string;
+  SameSite?: 'Strict' | 'Lax' | 'None';
+  HttpOnly?: boolean;
+  'Max-Age'?: number;
+  Expires?: string;
+  Domain?: string;
+};
+
+export const defaultCookieOptions: CookieOptions = {
+  Secure: true,
+  Path: '/',
+  SameSite: 'None',
+  Domain: '.gugbab.co.kr',
+};
+
+export const setCookies = (cookies: { key: string; value: string; options?: CookieOptions }[]) =>
+  cookies
+    .map(
+      ({ key, options, value }) =>
+        `${key}=${value}; ${Object.entries({ ...defaultCookieOptions, ...options })
+          .map(([key, value]) => `${key}=${value}`)
+          .join('; ')}`,
+    )
+    .join(', ');
