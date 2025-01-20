11fcc53d91457de67394568374172ab1c31155e7
puk0806 <puk0806@naver.com>
2025-01-20 10:46:18 +0900

    [all] Modify Readme, Package.json Description,git Hook

--- .githooks/pre-commit.py
+++ .githooks/pre-commit.py
@@ -3,16 +3,20 @@ import subprocess
 from git import get_changed_files
 
 changed_files = get_changed_files("ACMR")
-files_str = " ".join(changed_files)
 
-if not changed_files:
+allowed_exts = ('.js', '.jsx', '.ts', '.tsx')
+js_ts_files = [file for file in changed_files if file.endswith(allowed_exts)]
+
+files_str = " ".join(js_ts_files)
+
+if not js_ts_files:
     sys.exit(0)
 
 try:
-    files = " ".join([f"'{file}'" for file in changed_files])
+    files = " ".join([f"'{file}'" for file in js_ts_files])
     subprocess.run(f"npx eslint --fix {files}", shell=True, check=True)
 
-    subprocess.run(f"git add {files_str}", shell=True)
+    subprocess.run(f"git add {files_str}", shell=True, check=True)
 except subprocess.CalledProcessError as e:
     print(f"An error occurred: {e.output}")
-    exit(1)
+    sys.exit(1)
