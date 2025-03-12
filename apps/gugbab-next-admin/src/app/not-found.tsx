940de8a6e5968d2da9949e4b5c020310bbe81b2b
puk0806 <puk0806@naver.com>
2025-03-12 18:24:02 +0900

    [app] Move Error Ui Component Error Widget

--- apps/gugbab-next-admin/src/app/not-found.tsx
+++ apps/gugbab-next-admin/src/app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { ErrorContent } from '@app/shared/error';
+import { ErrorContent } from '@app/widgets/error';
 
 export default async function NotFound() {
   return <ErrorContent title="서비스 이용에 불편을 드려 죄송합니다." type={404} />;
