65593d95c21710835da6cf00ce5c7c30360b20fe
puk0806 <puk0806@naver.com>
2025-04-13 22:37:04 +0900

    [ui] Modify Chip Component Refactoring

--- packages/ui-sass/src/components/display/Chip/index.tsx
+++ packages/ui-sass/src/components/display/Chip/index.tsx
@@ -1,29 +1,46 @@
-import { memo } from 'react';
+'use client';
+
+import { ForwardedRef, ReactNode, forwardRef } from 'react';
 import { bem } from '@gugbab-integrated-admin-poc/utils';
-import { ChipProps } from '@types';
+import { TypographyProps } from '@types';
 import Typography from '../Typography';
 
 const cn = bem('chip');
 
-const Chip = ({ children, color = 'gray900', variant = 'filled' }: ChipProps) => {
-  return (
-    <span
-      className={cn(undefined, {
-        [variant]: !!variant,
-        [color]: !!color,
-      })}
-    >
-      {typeof children === 'string' ? (
-        <Typography component="em" variant="D2" weight="regular">
-          {children}
-        </Typography>
-      ) : (
-        children
-      )}
-    </span>
-  );
-};
+export interface ChipProps {
+  /** 외관 타입 정의 */
+  variant?: 'filled' | 'outlined' | 'light-outlined';
+  /** color 타입 정의 */
+  color?: 'red' | 'accent-red' | 'green' | 'navi' | 'negative-red' | 'orange' | 'gray900' | 'gray700';
+  /** Typography 속성 override */
+  typographyProps?: Omit<TypographyProps, 'children' | 'component'>;
+  /** 콘텐츠 */
+  children: ReactNode;
+}
+
+const Chip = forwardRef(
+  (
+    { children, color = 'gray900', typographyProps, variant = 'filled' }: ChipProps,
+    ref: ForwardedRef<HTMLSpanElement>,
+  ) => {
+    const classNames = cn(undefined, {
+      [variant]: !!variant,
+      [color]: !!color,
+    });
+
+    return (
+      <span className={classNames} ref={ref}>
+        {typeof children === 'string' ? (
+          <Typography component="em" variant="D2" weight="regular" {...typographyProps}>
+            {children}
+          </Typography>
+        ) : (
+          children
+        )}
+      </span>
+    );
+  },
+);
 
 Chip.displayName = 'Chip';
-
-export default memo(Chip);
+export default Chip;
