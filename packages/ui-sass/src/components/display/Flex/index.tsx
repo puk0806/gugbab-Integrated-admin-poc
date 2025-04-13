62a61d8a7686affc38fbbe2a866a689f719ea008
puk0806 <puk0806@naver.com>
2025-04-13 22:37:18 +0900

    [ui] Modify Flex Component Refactoring

--- packages/ui-sass/src/components/display/Flex/index.tsx
+++ packages/ui-sass/src/components/display/Flex/index.tsx
@@ -1,30 +1,53 @@
-import type { ElementType, CSSProperties } from 'react';
-import type { FlexComponent } from '@types';
+'use client';
+
+import type { ElementType, CSSProperties, HTMLAttributes, ReactNode } from 'react';
+import type { PolymorphicComponentProps } from '@types';
 import { forwardRef } from 'react';
 import { bem } from '@gugbab-integrated-admin-poc/utils';
 
 const cn = bem('flex');
 
+export interface FlexProps extends HTMLAttributes<HTMLElement> {
+  /** flex-direction: 'column' 활성화 */
+  vertical?: boolean;
+  /** flex-wrap properties */
+  wrap?: CSSProperties['flexWrap'];
+  /** justify-content properties */
+  justify?: CSSProperties['justifyContent'];
+  /** align-items properties */
+  align?: CSSProperties['alignItems'];
+  /** flex properties */
+  flex?: CSSProperties['flex'];
+  /** gap properties */
+  gap?: CSSProperties['gap'];
+  /** children */
+  children: ReactNode;
+}
+
+export type FlexComponent = <T extends ElementType = 'div'>(
+  props: PolymorphicComponentProps<T, FlexProps>,
+) => ReactNode;
+
 const Flex: FlexComponent = forwardRef(
-  ({ align = 'center', children, component, flex, gap, justify, style, vertical, wrap }, ref) => {
+  ({ align = 'center', children, component, flex, gap, justify, style, vertical, wrap, ...rest }, ref) => {
     const Component: ElementType = component || 'div';
 
-    const mergedStyle: CSSProperties = {
-      ...style,
+    const styles: CSSProperties = {
       gap,
       alignItems: align,
       justifyContent: justify,
       flexDirection: vertical ? 'column' : 'row',
       flexWrap: wrap,
-      flex: flex,
+      flex,
+      ...style,
     };
 
     return (
-      <Component className={cn()} ref={ref} style={mergedStyle}>
+      <Component className={cn()} ref={ref} style={styles} {...rest}>
         {children}
       </Component>
     );
   },
 );
 
-export default Flex as FlexComponent;
+export default Flex;
