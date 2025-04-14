b9e9e0db3b0ad8f22a9c542fd72fb9b3bbaeed83
puk0806 <puk0806@naver.com>
2025-04-15 08:29:50 +0900

    [ui] Modify Loading Component

--- packages/ui-sass/src/components/display/Loading/index.tsx
+++ packages/ui-sass/src/components/display/Loading/index.tsx
@@ -1,30 +1,60 @@
+'use client';
+
+import type { ReactNode } from 'react';
+import type { TypographyProps } from '@types';
 import { bem } from '@gugbab-integrated-admin-poc/utils';
-import { memo, useEffect, useMemo, useState } from 'react';
-import { LoadingProps, TypographyProps } from '@types';
+import { useEffect, useState } from 'react';
 import Ir from '../../utils/Ir';
 import Typography from '../Typography';
 
 const cn = bem('loading');
 
+interface LoadingBaseProps {
+  /** loading title  */
+  title: string;
+  /** loading description  */
+  desc?: ReactNode;
+  /** box 사용여부  */
+  removeBox?: boolean;
+  /** remove dimmed 처리 */
+  removeDimmed?: boolean;
+}
+
+interface LoadingDefaultProps extends LoadingBaseProps {
+  /** progressbar type  */
+  variant?: 'none' | 'spinner';
+  /** progressbar % */
+  percent?: never;
+}
+
+interface LoadingProgressBarProps extends LoadingBaseProps {
+  /** progressbar type  */
+  variant?: 'progressbar';
+  /** progressbar % */
+  percent: number;
+}
+
+export type LoadingProps = LoadingDefaultProps | LoadingProgressBarProps;
+
 const ProgressBar = ({ percent = 0 }: { percent?: number }) => {
   return (
     <div className={cn('progressbar')}>
-      <span style={{ transform: `translateX(${percent - 100}%)` }}></span>
-      <Ir>{`${percent.toString()}% 로딩 중`}</Ir>
+      <span style={{ transform: `translateX(${percent - 100}%)` }} />
+      <Ir>{`${percent}% 로딩 중`}</Ir>
     </div>
   );
 };
 
-const Spinner = memo(() => {
+const Spinner = () => {
   return (
     <div className={cn('spinner')}>
-      <span></span>
-      <span></span>
-      <span></span>
+      <span />
+      <span />
+      <span />
       <Ir>로딩 중</Ir>
     </div>
   );
-});
+};
 
 const Loading = ({
   desc,
@@ -36,67 +66,20 @@ const Loading = ({
 }: LoadingProps) => {
   const [image, setImage] = useState<string>();
 
-  const titleProps = useMemo<TypographyProps>(
-    () =>
-      removeBox
-        ? {
-            color: 'white',
-            variant: 'H3',
-          }
-        : {
-            color: 'grayscale-gray900',
-            variant: 'B1',
-            weight: 'bold',
-          },
-    [removeBox],
-  );
+  const titleProps: TypographyProps = removeBox
+    ? { color: 'white', variant: 'H3' }
+    : { color: 'grayscale-gray900', variant: 'B1', weight: 'bold' };
 
-  const descProps = useMemo<TypographyProps>(
-    () =>
-      removeBox
-        ? {
-            color: 'white',
-            variant: 'B2',
-          }
-        : {
-            color: 'grayscale-gray900',
-            variant: 'B2',
-          },
-    [removeBox],
-  );
-
-  const loadingContent = useMemo(
-    () => (
-      <>
-        <div className={cn('message')}>
-          <Typography component="p" {...titleProps}>
-            {title}
-          </Typography>
-          {desc && (
-            <Typography component="p" {...descProps}>
-              {desc}
-            </Typography>
-          )}
-        </div>
-
-        <div className={cn('animation')}>
-          {image && (
-            <>
-              <img alt="" src={image} srcSet={`${image} 1x, ${image} 2x, ${image} 3x`} />
-            </>
-          )}
-        </div>
-      </>
-    ),
-    [desc, descProps, image, title, titleProps],
-  );
+  const descProps: TypographyProps = removeBox
+    ? { color: 'white', variant: 'B2' }
+    : { color: 'grayscale-gray900', variant: 'B2' };
 
   useEffect(() => {
-    (async function () {
+    (async () => {
       try {
-        const loading = await import(`./img/loading.gif`);
+        const loading = await import('./img/loading.gif');
         setImage(loading.default);
-      } catch (error) {
+      } catch {
         throw new Error('gif가 정상적으로 로딩되지 않았습니다.');
       }
     })();
@@ -110,10 +93,26 @@ const Loading = ({
     >
       <div
         className={cn('content', {
-          ['remove-box']: removeBox,
+          'remove-box': removeBox,
         })}
       >
-        <div className={cn('loader')}>{loadingContent}</div>
+        <div className={cn('loader')}>
+          <div className={cn('message')}>
+            <Typography component="p" {...titleProps}>
+              {title}
+            </Typography>
+            {desc && (
+              <Typography component="p" {...descProps}>
+                {desc}
+              </Typography>
+            )}
+          </div>
+        </div>
+        {variant !== 'spinner' && (
+          <div className={cn('animation')}>
+            {image && <img alt="" src={image} srcSet={`${image} 1x, ${image} 2x, ${image} 3x`} />}
+          </div>
+        )}
         {variant === 'progressbar' && <ProgressBar percent={percent} />}
         {variant === 'spinner' && <Spinner />}
       </div>
@@ -122,4 +121,4 @@ const Loading = ({
 };
 
 Loading.displayName = 'Loading';
-export default memo(Loading);
+export default Loading;
