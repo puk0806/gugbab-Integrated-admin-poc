e6b22c4783bf1aeaee82352aa91095f50c78f29a
puk0806 <puk0806@naver.com>
2025-04-14 17:01:58 +0900

    [ui] Modify Image Compoent Refactoring

--- packages/ui-sass/src/components/display/Image/index.tsx
+++ packages/ui-sass/src/components/display/Image/index.tsx
@@ -1,11 +1,39 @@
-import type { RefObject } from 'react';
+'use client';
+
+import type { CSSProperties, ImgHTMLAttributes, RefObject } from 'react';
+import { useEffect, useId, useRef, useState, useMemo } from 'react';
 import { bem, calculateRatio, stringifyQuery } from '@gugbab-integrated-admin-poc/utils';
-import { memo, useCallback, useEffect, useId, useMemo, useRef, useState } from 'react';
-import { ImageProps } from '@types';
 import useIntersection from '@hooks/useIntersection';
 import errorDefaultImg from './img/error-default.svg';
 
 const cn = bem('custom-image');
+
+type ImageLayout = 'intrinsic' | 'fixed' | 'responsive' | 'fill';
+
+export interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+  /** image url */
+  src: string;
+  /** image alt */
+  alt: string;
+  /** image width (layout props가 `responsive`, `fill` 의 경우 이미지 자체의 원본 사이즈 입력)<br />
+   * `goggle page speed 정책반영` 필수 입력으로 전환
+   * */
+  width: number | string;
+  /** image height (layout props가 `responsive`, `fill` 의 경우 이미지 자체의 원본 사이즈 입력)<br />
+   * `goggle page speed 정책반영` 필수 입력으로 전환
+   * */
+  height: number | string;
+  /** lazy loading 여부 */
+  isLazy?: boolean;
+  /** image object fit property */
+  objectFit?: CSSProperties['objectFit'];
+  /** 에러이미지 경로 */
+  errorSrc?: string;
+  /** 이미지 레이아웃 */
+  layout?: ImageLayout;
+  /** Cache busting용 쿼리 추가 */
+  bustCache?: boolean;
+}
 
 const Image = ({
   alt,
@@ -20,8 +48,8 @@ const Image = ({
 }: ImageProps) => {
   const uniqueId = useId();
   const [isInView, setIsInView] = useState(false);
-  const imgRef = useRef<HTMLImageElement>(null);
   const [isError, setIsError] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
 
   useIntersection(imgRef as RefObject<HTMLImageElement>, () => setIsInView(true));
 
@@ -35,16 +63,25 @@ const Image = ({
     return `${src}${stringifyQuery({ v: uniqueId }, true, src?.includes('?') ? '&' : '?')}`;
   }, [bustCache, isInView, isLazy, src, uniqueId]);
 
-  const handleError = useCallback(() => {
-    if (!(imgRef.current instanceof HTMLImageElement)) return;
-    imgRef.current.src = errorSrc;
-    setIsError(true);
-    console.warn(`${formattedSource} has been replaced to ${errorSrc}`);
-  }, [errorSrc, formattedSource]);
-
   useEffect(() => {
-    imgRef.current?.addEventListener('error', handleError);
-  }, [handleError]);
+    if (!imgRef.current) {
+      return;
+    }
+
+    const handleError = () => {
+      if (!(imgRef.current instanceof HTMLImageElement)) return;
+      imgRef.current.src = errorSrc;
+      setIsError(true);
+      console.warn(`${formattedSource} has been replaced to ${errorSrc}`);
+    };
+
+    imgRef.current.addEventListener('error', handleError);
+    return () => {
+      if (imgRef.current) {
+        imgRef.current.removeEventListener('error', handleError);
+      }
+    };
+  }, [errorSrc, formattedSource]);
 
   if (layout === 'fixed') {
     return (
@@ -61,7 +98,11 @@ const Image = ({
 
   return (
     <span
-      className={cn(undefined, { [layout]: !!layout, ['has-ratio']: !!props.width && !!props.height, error: isError })}
+      className={cn(undefined, {
+        [layout]: !!layout,
+        'has-ratio': !!props.width && !!props.height,
+        error: isError,
+      })}
       style={{
         aspectRatio: calculateRatio(props.width, props.height),
       }}
@@ -71,4 +112,4 @@ const Image = ({
   );
 };
 
-export default memo(Image);
+export default Image;
