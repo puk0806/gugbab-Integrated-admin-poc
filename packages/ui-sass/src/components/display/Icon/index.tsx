422688e63d315d9a0dbeb724185a2d2f11221792
puk0806 <puk0806@naver.com>
2025-04-14 17:01:36 +0900

    [ui] Modify Icon Compoent Refactoring

--- packages/ui-sass/src/components/display/Icon/index.tsx
+++ packages/ui-sass/src/components/display/Icon/index.tsx
@@ -1,28 +1,45 @@
+'use client';
+
 import type { CSSProperties } from 'react';
 import { memo } from 'react';
 import { bem } from '@gugbab-integrated-admin-poc/utils';
-import { IconProps } from '@types';
-import { Ir } from '../../../index';
+import icons from '@gugbab-integrated-admin-poc/icons';
+import { ColorTypes } from '@types';
+import { Ir } from '../../..';
+
+const cn = bem('icon');
+
+export interface IconProps {
+  /** 아이콘 클래스명 */
+  name: (typeof icons)[number];
+  /** icon fontsize(px) */
+  size?: number;
+  /** icon color(#de2231) */
+  color?: ColorTypes;
+  /** 웹접근성 위한 아이콘 이름 */
+  irName?: string;
+}
 
 const Icon = ({ color, irName, name, size = 24 }: IconProps) => {
-  const cn = bem('icon');
-  const style: CSSProperties = {};
+  const styles: CSSProperties = {
+    width: `${size}px`,
+    height: `${size}px`,
+    backgroundSize: `${size}px auto`,
+    fontSize: `${size}px`,
+  };
 
-  if (size) {
-    style.width = `${size}px`;
-    style.height = `${size}px`;
-    style.backgroundSize = `${size}px auto`;
-    style.fontSize = `${size}px`;
-  }
+  const className = cn(undefined, {
+    [name]: true,
+    [`${color}`]: !!color,
+  });
 
   return (
     <>
-      <i className={cn(undefined, { [name]: name, [`${color}`]: !!color })} style={{ ...style }} />
+      <i className={className} style={styles} />
       {irName && <Ir>{irName}</Ir>}
     </>
   );
 };
 
 Icon.displayName = 'Icon';
-
 export default memo(Icon);
