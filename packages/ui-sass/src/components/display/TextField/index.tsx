1ba9a63ad37a5a04b38a20b951cf81a5240a2808
puk0806 <puk0806@naver.com>
2025-04-16 08:29:29 +0900

    [ui] Modify TextFiled Component

--- packages/ui-sass/src/components/display/TextField/index.tsx
+++ packages/ui-sass/src/components/display/TextField/index.tsx
@@ -3,10 +3,9 @@ import React, {
   FocusEvent,
   ForwardedRef,
   forwardRef,
-  memo,
+  InputHTMLAttributes,
   MouseEventHandler,
   ReactNode,
-  useCallback,
   useId,
   useMemo,
   useRef,
@@ -16,11 +15,47 @@ import { Controller, useController } from 'react-hook-form';
 import { Noop } from 'react-hook-form/dist/types';
 import { flushSync } from 'react-dom';
 import { bem, changePhoneNumber } from '@gugbab-integrated-admin-poc/utils';
-import { TextFieldProps } from '@types';
 import Typography from '../Typography';
 import Icon from '../Icon';
 
 const cn = bem('text-field');
+
+export interface TextFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  /** input inline width */
+  width?: string;
+  /** input title string */
+  title?: string;
+  /** label string */
+  label?: string;
+  /** text field text align right */
+  rightAlign?: boolean;
+  /** text field name */
+  name: string;
+  /** validation error message */
+  error?: string;
+  /** validation success message */
+  success?: string;
+  /** clear button 활성화 ( `keep : value가 있을경우 유지`, `focus : focus시에만 노출`, `search : search icon 과 clear btn change` ) */
+  clearable?: 'keep' | 'focus' | 'search';
+  /** Reverse */
+  reverse?: boolean;
+  /** number Type */
+  numberType?: 'phone' | 'business' | 'number' | 'comma';
+  /** text field utils */
+  utils?: ReactNode;
+  /** react hook form control */
+  control?: any;
+  /** react hook form setValue */
+  setValue?: any;
+  /** react hook form rules */
+  rules?: any;
+  /** text field 우측 추가 button */
+  button?: ReactNode;
+  /** text field 우측 추가 button */
+  onChange?: (e: ChangeEvent<HTMLInputElement>, value: string) => void;
+  /** text field 하단 추가 children */
+  children?: ReactNode;
+}
 
 const TextField = forwardRef(
   (
@@ -59,7 +94,7 @@ const TextField = forwardRef(
     const fieldValue = control ? useController({ control, name, rules }) : null;
 
     // input set value(maxLength 한글 방지용)
-    const setInputValue = useCallback((value: string) => {
+    const setInputValue = (value: string) => {
       const rootDom = rootDomRef.current as HTMLDivElement;
       const input = rootDom.querySelector('input');
       let nativeInputValueSetter: any = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value');
@@ -68,26 +103,23 @@ const TextField = forwardRef(
         nativeInputValueSetter.call(input, value);
       }
       return input;
-    }, []);
+    };
 
-    const parseValue = useCallback(
-      (val: string) => {
-        if (numberType === 'phone' && val) {
-          return changePhoneNumber(val);
-        } else if (numberType === 'business' && val) {
-          val = val.replace(/[^0-9]/g, '').replace(/-/g, '');
-          val = val.substring(0, 10);
-          return val.replace(/^(\d{0,3})(\d{0,2})(\d{0,5})$/g, '$1-$2-$3').replace(/-{1,2}$/g, '');
-        } else if (numberType === 'number' && val) {
-          return val.replace(/[^0-9]/g, '').replace(/-/g, '');
-        } else if (numberType === 'comma' && val) {
-          const returnValue = val.replace(/[^0-9]/g, '').replace(/-/g, '');
-          return Number(returnValue).toLocaleString('ko-KR');
-        }
-        return val;
-      },
-      [numberType],
-    );
+    const parseValue = (val: string) => {
+      if (numberType === 'phone' && val) {
+        return changePhoneNumber(val);
+      } else if (numberType === 'business' && val) {
+        val = val.replace(/[^0-9]/g, '').replace(/-/g, '');
+        val = val.substring(0, 10);
+        return val.replace(/^(\d{0,3})(\d{0,2})(\d{0,5})$/g, '$1-$2-$3').replace(/-{1,2}$/g, '');
+      } else if (numberType === 'number' && val) {
+        return val.replace(/[^0-9]/g, '').replace(/-/g, '');
+      } else if (numberType === 'comma' && val) {
+        const returnValue = val.replace(/[^0-9]/g, '').replace(/-/g, '');
+        return Number(returnValue).toLocaleString('ko-KR');
+      }
+      return val;
+    };
 
     // default onChange
     const handleChange = (e: ChangeEvent<HTMLInputElement>, fieldChange?: (...event: any[]) => void) => {
@@ -109,56 +141,46 @@ const TextField = forwardRef(
     };
 
     // reset handler
-    const handleReset: MouseEventHandler<HTMLButtonElement> = useCallback(
-      e => {
-        const input = setInputValue('');
-        if (input) {
-          input.focus();
-          input.dispatchEvent(new Event('input', { bubbles: true }));
-        }
-        setValue?.(name, '');
-      },
-      [name, setInputValue, setValue],
-    );
+    const handleReset: MouseEventHandler<HTMLButtonElement> = e => {
+      const input = setInputValue('');
+      if (input) {
+        input.focus();
+        input.dispatchEvent(new Event('input', { bubbles: true }));
+      }
+      setValue?.(name, '');
+    };
 
-    const handleFocus = useCallback(() => {
+    const handleFocus = () => {
       if (disabled || readOnly) return;
       setFocus(true);
-    }, [disabled, readOnly]);
+    };
 
-    const handleBlur = useCallback(
-      (e: FocusEvent<HTMLInputElement>, fieldBlur?: Noop) => {
-        setFocus(false);
-        fieldBlur && fieldBlur();
-        props?.onBlur?.(e);
-      },
-      [props],
-    );
+    const handleBlur = (e: FocusEvent<HTMLInputElement>, fieldBlur?: Noop) => {
+      setFocus(false);
+      fieldBlur && fieldBlur();
+      props?.onBlur?.(e);
+    };
 
     const hasValue = useMemo(() => {
       if (fieldValue) {
-        return fieldValue?.field?.value && fieldValue?.field?.value?.length > 0;
+        return typeof fieldValue.field.value === 'string' && fieldValue.field.value.length > 0;
       }
-      return props.value && String(props.value)?.length > 0;
-    }, [fieldValue, props.value]);
+      return typeof props.value === 'string' && props.value.length > 0;
+    }, [fieldValue?.field?.value, props.value]);
 
     return (
       <div className={cn()} style={{ width }}>
         <div className={cn('container', { focus: !disabled && (hasValue || focus), transform: !!label })}>
           <div
             className={cn('field', {
-              ['error']: !!error,
-              ['reverse']: !!reverse,
-              ['disabled']: !!disabled,
-              ['readonly']: !!readOnly,
+              error: !!error,
+              reverse: !!reverse,
+              disabled: !!disabled,
+              readonly: !!readOnly,
             })}
           >
             <div className={cn('input')} ref={rootDomRef}>
-              <div
-                className={cn('control', {
-                  ['right-align']: !!rightAlign,
-                })}
-              >
+              <div className={cn('control', { 'right-align': !!rightAlign })}>
                 {label && (
                   <div className={cn('label', { transform: !!label })}>
                     <Typography
@@ -216,25 +238,19 @@ const TextField = forwardRef(
                   />
                 )}
               </div>
+
               {!readOnly && ((clearable === 'keep' && hasValue) || clearable === 'focus') && (
-                <button
-                  type="button"
-                  className={cn('clear', {
-                    [`${clearable}`]: clearable,
-                  })}
-                  onClick={handleReset}
-                >
+                <button className={cn('clear', { [`${clearable}`]: clearable })} type="button" onClick={handleReset}>
                   <Icon irName="입력값 지우기" name="close" size={IconSize} />
                 </button>
               )}
+
               {!readOnly && clearable === 'search' && (
                 <>
                   {hasValue ? (
                     <button
+                      className={cn('clear', { [`${clearable}`]: clearable })}
                       type="button"
-                      className={cn('clear', {
-                        [`${clearable}`]: clearable,
-                      })}
                       onClick={handleReset}
                     >
                       <Icon irName="입력값 지우기" name="close" size={IconSize} />
@@ -248,18 +264,17 @@ const TextField = forwardRef(
 
             {utils && <div className={cn('utils')}>{utils}</div>}
           </div>
+
           {button && (
             <div className={cn('button')}>
               {React.Children.map<ReactNode, ReactNode>(button, child => {
                 if (!React.isValidElement(child)) return null;
-                const validChild = child as React.ReactElement<any>;
-                return React.cloneElement(validChild as React.ReactElement<any>, {
-                  ...validChild.props,
-                });
+                return React.cloneElement(child);
               })}
             </div>
           )}
         </div>
+
         {(error || success || children) && (
           <div>
             {error && (
@@ -281,4 +296,4 @@ const TextField = forwardRef(
 );
 
 TextField.displayName = 'TextField';
-export default memo(TextField);
+export default TextField;
